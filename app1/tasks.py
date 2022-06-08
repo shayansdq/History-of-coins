672793683393ba3757685d4889566b68a9ffebe5
Shayan <shayansadeghi178@gmail.com>
2022-06-09 02:23:11 +0430

    set run tasks asynchronously and separated separate sections

--- app1/tasks.py
+++ app1/tasks.py
@@ -1,36 +1,42 @@
 from __future__ import absolute_import, unicode_literals
-import pandas as pd
-import environ
-from binance import Client as binance_client
-from datetime import datetime
 from celery import shared_task
 from django_redis import get_redis_connection
-import pickle
-import zlib
 from .utils import *
+import asyncio
 
 env = environ.Env()
 environ.Env.read_env()
 # symbols = env('SYMBOLS').split(',')
 exchanges = env('EXCHANGES').split(',')
-client = binance_client()
+# client = binance_client()
+api_key = '<api_key>'
+api_secret = '<api_secret>'
 cache = get_redis_connection('default')
 
 
-# def binance(sy
+async def binance(candle_data):
+    exchange = 'binance'
+    symbols = env(f'{exchange.upper()}_SYMBOLS').split(',')
+    candle_type = candle_data['type']
+    candle_time = candle_data['time']
+    client = await async_binance_client.create(api_key, api_secret)
+    await asyncio.gather(*[get_binance_data(client, symbol, candle_type, candle_time) for symbol in symbols])
+    # await asyncio.gather(*[kucoin(client, symbol, candle_type, candle_time) for symbol in symbols])
+    await client.close_connection()
+
+
+def kucoin(candle_data):
+    candle_type = candle_data['type']
+    candle_time = candle_data['time']
+    kucoin_symbols = env('KUCOIN_SYMBOLS').split(',')
+    for symbol in kucoin_symbols:
+        created_df = get_kucoin_data(symbol, candle_type, candle_time)
+        compressed_df = zlib.compress(pickle.dumps(created_df))
+        cache.set(f"kucoin-{symbol}-{candle_time}-{candle_type}", compressed_df)
 
 
 @shared_task
 def get_data(**kwargs):
     candle_data = kwargs['candle_type']
-    candle_type = candle_data['type']
-    candle_time = candle_data['time']
-    print(candle_type, candle_time)
-    for exchange in exchanges:
-        symbols = env(f'{exchange.upper()}_SYMBOLS').split(',')
-        for symbol in symbols:
-            last_candles_data = eval(f"{exchange}('{symbol}','{candle_type}','{candle_time}')")
-            created_df = eval(f"create_{exchange}_df(last_candles_data)")
-            compressed_df = zlib.compress(pickle.dumps(created_df))
-            print(created_df)
-            cache.set(f"{exchange}-{symbol}-{candle_time}-{candle_type}", compressed_df)
+    asyncio.run(binance(candle_data))
+    kucoin(candle_data)
