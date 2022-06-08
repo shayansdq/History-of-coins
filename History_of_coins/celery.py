f2e4006c881a6997b3ba498b26d8f5e833c7d383
Shayan <shayansadeghi178@gmail.com>
2022-06-08 18:14:07 +0430

    config and create our celery app

--- History_of_coins/celery.py
+++ History_of_coins/celery.py
@@ -1,9 +1,20 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from django.conf import settings
-from celery import Celery
+from __future__ import absolute_import
 
+import os
+import sys
+
+from celery import Celery
+from django.conf import settings
+import django
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'History_of_coins.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
+
+django.setup()
 
 app = Celery('History_of_coins')
 
